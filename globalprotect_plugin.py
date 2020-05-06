b76bcbf3f326ad6f5e5e336910b59fd5f0d2ba31
Irek Romaniuk <irekromaniuk@Ireks-Mac-mini.local>
2020-05-06 14:21:18 -0400

    version 1.1 with new names

--- globalprotect_plugin.py
+++ globalprotect_plugin.py
@@ -11,23 +11,19 @@ class GPPluginRemote(RemoteBasePlugin):
         # self.path= self.config["path"]
         self.api= self.config.get("API", '')
         self.url= self.config.get("url", "https://10.1.3.6")
-        logger.info("API: %s, URL: %s", self.api, self.url)
+        self.name= self.config.get("name", "paneastusprod1")
+        logger.info("API: %s, URL: %s, NAME: %s", self.api, self.url, self.name)
         
 
     def query(self, **kwargs):
-        device = {
-        'host': "device_host",
-        'type': "device_type",
-        'group': "device_group"
-        }
         # Create the group/device entities in Dynatrace
-        group_name = "GroupName"
+        group_name = "GlobalProtect"
         group = self.topology_builder.create_group(group_name, group_name)
-        device_name = "DeviceName"
+        device_name = self.name
         device = group.create_device(device_name, device_name)
         logger.info("Topology: group name=%s, device name=%s", group.name, device.name)
         # Provide user count
-        logger.info("URL = %s", self.url + self.PATH)
+        # logger.info("URL = %s", self.url + self.PATH)
         userscount = self.users_count(self.url + self.PATH)
         logger.info("Number of users = %s", userscount)
         device.absolute(key='userscount', value=userscount)
@@ -36,6 +32,8 @@ class GPPluginRemote(RemoteBasePlugin):
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         r = requests.get(url = url + self.api, verify=False)
         doc = json.loads(json.dumps(xmltodict.parse(r.text))) 
+        entries = []
         if 'success' in doc['response']['@status']:
-            entries = doc['response']['result']['entry'] 
-        return len(entries)            
+            entries = doc['response']['result']['entry']   
+        return len(entries)    
+                  
